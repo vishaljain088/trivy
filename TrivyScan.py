8820428845bfe7671ef7bc4cd4d80831b27d9b41
Vishal Jain <vishaljain088@hotmail.com>
2022-03-23 01:47:03 +0530

    Update TrivyScan.py

--- TrivyScan.py
+++ TrivyScan.py
@@ -1,6 +1,6 @@
 import json
 
-with open('trivyreport.json', encoding="utf") as jsonfile:
+with open('trivyreport.json') as jsonfile:
     jsondata = ''.join(line for line in jsonfile if not line.startswith('2022' or '2023'))
     res = json.loads(jsondata)
     for r in res["Results"]:
